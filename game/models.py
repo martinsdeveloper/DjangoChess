37419136651badde7954b2238dc49df1a64f9a2d
Martins Darguzis <martins.darguzis@catchsmart.com>
2022-04-12 11:50:58 +0300

    WIP chess piece movement

--- game/models.py
+++ game/models.py
@@ -9,31 +9,18 @@ import numpy as np
 
 @dataclass
 class ChessBoard:
-    moves: dict
-    add_moves: dict
     board: list[int] = field(default_factory=list)
     def zeros(self, range):
         for _ in range:
             yield -1
     def empty_board(self):
         for _ in range(8): 
-            yield list(self.zeros(range(1,8)))
+            yield list(self.zeros(range(0,8)))
     def __init__(self):
         self.board = list(self.empty_board())
-        # self.moves =  {"Pawn": [[0,1]], "Knigt": [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,-1],[-2,1]]}
-        # self.add_moves = {"Pawn": [[0,2]] }
-    # def put_in_position(self, type, x, y):
-    #     self.board = list(self.empty_board())
-    #     self.board[x][y] = 1
-    #     for (xd,yd) in self.moves[type]:
-    #         self.board[x+xd][y+yd] = 0
-    #     return self
-    # def additional_moves(self, type, x, y):
-    #     self.board = self.put_in_position(type,x,y).board
-    #     for (xd,yd) in self.add_moves[type]:
-    #         self.board[x+xd][y+yd] = 0
-    #     print("additional moves")
-    #     return self
+    @property
+    def clear_board(self):
+         self.board = list(self.empty_board())
     @property
     def print(self):
         for row in self.board:
@@ -50,8 +37,7 @@ class Knigt:
         self.y = init_y
         self.moves = ChessBoard()
     def available_moves(self):
-        # self.moves = ChessBoard().put_in_position(self.__class__.__name__, self.x, self.y)
-        # def set_available_moves(self):
+        self.moves.clear_board
         self.moves.board[self.x][self.y] = 1
         for i in (1,-1):
             for n in (2,-2):
@@ -73,16 +59,160 @@ class Pawn:
     def __init__(self,init_x = 0, init_y = 0):
         self.x = init_x
         self.y = init_y
+        self.moves = ChessBoard()
     def available_moves(self):
+        self.moves.clear_board
+        self.moves.board[self.x][self.y] = 1
         if (self.last_moved == 0):
-            self.moves = ChessBoard().additional_moves(self.__class__.__name__, self.x, self.y)
-        elif (1==2):
-            print("passe")
+            self.moves.board[self.x][1+self.y] = 0
+            self.moves.board[self.x][2+self.y] = 0
         else:
-            self.moves = ChessBoard().put_in_position(self.__class__.__name__, self.x, self.y)
+            self.moves.board[self.x][1+self.y] = 0
+            if (1==2):
+                print("passe")
     def move(self,move_to_x,move_to_y,turn_no):
         self.x = move_to_x
         self.y = move_to_y
         self.last_moved = turn_no
         self.available_moves()
 
+@dataclass
+class Rook:
+    moves: ChessBoard
+    last_moved: int = 0
+    value: int = 4
+    x: int = 0
+    y: int = 0
+    def __init__(self,init_x = 0, init_y = 0):
+        self.x = init_x
+        self.y = init_y
+        self.moves = ChessBoard()
+    def available_moves(self):
+        self.moves.clear_board
+        for i in range(0,8):
+            self.moves.board[self.x][i] = 0
+        for n in range(0,8):
+            self.moves.board[n][self.y] = 0
+        self.moves.board[self.x][self.y] = 1
+    def move(self,move_to_x,move_to_y,turn_no):
+        self.x = move_to_x
+        self.y = move_to_y
+        self.last_moved = turn_no
+        self.available_moves()
+
+# rook = Rook()
+# rook.move(0,0,0)
+# rook.moves.print
+
+@dataclass
+class Bishop:
+    moves: ChessBoard
+    last_moved: int = 0
+    value: int = 9
+    x: int = 0
+    y: int = 0
+    def __init__(self,init_x = 0, init_y = 0):
+        self.x = init_x
+        self.y = init_y
+        self.moves = ChessBoard()
+    def available_moves(self):
+        self.moves.clear_board
+        for i in range(0,8):
+            if (self.x+i < 8):
+                if (self.y+i < 8):
+                    self.moves.board[self.x+i][self.y+i] = 0
+                if (self.y-i >= 0):
+                    self.moves.board[self.x+i][self.y-i] = 0
+            if (self.x-i >= 0):
+                if (self.y+i < 8):
+                    self.moves.board[self.x-i][self.y+i] = 0
+                if (self.y-i >= 0):
+                    self.moves.board[self.x-i][self.y-i] = 0
+        self.moves.board[self.x][self.y] = 1
+    def move(self,move_to_x,move_to_y,turn_no):
+        self.x = move_to_x
+        self.y = move_to_y
+        self.last_moved = turn_no
+        self.available_moves()
+
+# bishop = Bishop()
+# bishop.move(3,3,0)
+# bishop.moves.print
+
+@dataclass
+class Queen:
+    moves: ChessBoard
+    last_moved: int = 0
+    value: int = 9
+    x: int = 0
+    y: int = 0
+    def __init__(self,init_x = 0, init_y = 0):
+        self.x = init_x
+        self.y = init_y
+        self.moves = ChessBoard()
+    def available_moves(self):
+        self.moves.clear_board
+        for i in range(0,8):
+            if (self.x+i < 8):
+                if (self.y+i < 8):
+                    self.moves.board[self.x+i][self.y+i] = 0
+                if (self.y-i >= 0):
+                    self.moves.board[self.x+i][self.y-i] = 0
+            if (self.x-i >= 0):
+                if (self.y+i < 8):
+                    self.moves.board[self.x-i][self.y+i] = 0
+                if (self.y-i >= 0):
+                    self.moves.board[self.x-i][self.y-i] = 0
+        for i in range(0,8):
+            self.moves.board[self.x][i] = 0
+        for n in range(0,8):
+            self.moves.board[n][self.y] = 0
+        self.moves.board[self.x][self.y] = 1
+    def move(self,move_to_x,move_to_y,turn_no):
+        self.x = move_to_x
+        self.y = move_to_y
+        self.last_moved = turn_no
+        self.available_moves()
+
+@dataclass
+class King:
+    moves: ChessBoard
+    last_moved: int = 0
+    value: int = 1
+    x: int = 0
+    y: int = 0
+    def __init__(self,init_x = 0, init_y = 0):
+        self.x = init_x
+        self.y = init_y
+        self.moves = ChessBoard()
+    def available_moves(self):
+        self.moves.clear_board
+        if (self.last_moved == 0):
+            # castling when team added
+            print("castling")
+        else:
+            if self.x > 0 :
+                # kreisaa
+                if self.y - 1 >= 0 :
+                    self.moves.board[self.x-1][self.y-1] = 0
+                self.moves.board[self.x-1][self.y] = 0
+                if self.y + 1 < 8 :
+                    self.moves.board[self.x-1][self.y+1] = 0
+            if self.x < 8 :
+                # labaa
+                if self.y - 1 >= 0 :
+                    self.moves.board[self.x][self.y-1] = 0
+                self.moves.board[self.x][self.y] = 0
+                if self.y + 1 < 8 :
+                    self.moves.board[self.x][self.y+1] = 0
+        self.moves.board[self.x][self.y] = 1
+    def move(self,move_to_x,move_to_y,turn_no):
+        self.x = move_to_x
+        self.y = move_to_y
+        self.last_moved = turn_no
+        self.available_moves()
+
+# k = King()
+# k.move(3,1,2)
+# k.moves.print
+# # https://www.chess.com/terms/chess-pieces
